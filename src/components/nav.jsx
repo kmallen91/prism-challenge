1f785a431348d59609c58858353e24c4bc9c63ba
Kevin Allen <kevin.allen91@gmail.com>
2020-06-14 23:51:09 -0700

    set up router for nav links, no components connected

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../datatonic-logo-03.svg'
 import '../styles/navbar.css'
+import {NavLink} from 'react-router-dom'
 
 export default function Navbar() {
 
@@ -9,10 +10,26 @@ export default function Navbar() {
         <div className='nav-container'>
             <ul className='nav-list'>
                 <li className='logo'> <img className='logo-image'  src={logo} alt='datatonic logo' /></li>
-                <li className='list-item'> Dashboard </li>
-                <li className='list-item'> Sources </li>
-                <li className='list-item'> Inspector </li>
-                <li className='list-item'> Ingestion </li>
+                <li>
+                <NavLink to="/dashboard" className='nav-item'>
+                    Dashboard
+                </NavLink>
+                </li>
+                <li>
+                <NavLink to="/sources" className='nav-item'>
+                    Sources
+                </NavLink>
+                </li>
+                <li>
+                <NavLink to="/inspector" className='nav-item'>
+                    Inspector
+                </NavLink>
+                </li>
+                <li>
+                <NavLink to="/ingestion" className='nav-item'>
+                    Ingestion
+                </NavLink>
+                </li>
             </ul>
             <div className='nav-item'>User001</div>
         </div>
